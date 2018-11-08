1397bf1e6565ef30efc66262855703bf8e02b8e8
tferreiraw <thiago.ferreira@luizalabs.com>
2018-11-08 09:02:57 -0200

    First commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount, render } from 'enzyme';
+import toJson from "enzyme-to-json"
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it("renders with enzyme", () => {
+  const app = mount(<App />)
+  expect(toJson(app)).toMatchSnapshot()
+})
